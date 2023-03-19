77706dd6b61e0e35e6a35e5aef29b2c4fa97d8fe
Zuleykha22 <zuleyxahasanova22@gmail.com>
2023-03-19 11:38:08 +0400

    first commit

---
+++ lesson.py
@@ -0,0 +1,254 @@
+# print('Hello World!')
+
+# print('Hello World', 456)
+# print('Hello World', 54)
+# print('Hello World', 5654)
+
+# print('Hello World', 45475) # This code is imporant 
+
+"""
+First lessons topics:
+fsdf
+df
+dgf
+gfg
+fg
+"""
+# print('Hello World!')
+# print(5 + 10)
+# print(150 + 85 * 236)
+
+# greeting = 'Hello World!' # variable 
+# print(greeting)
+# a = 5
+# b = 15
+# print(a * b) 
+
+# first_number = 1
+# number_1 = 1
+# number1 = 52
+
+# print(a * 5)
+
+'''
+Data tipleri:
+String;
+
+Numeric data tipleri:
+int (integer)
+float
+'''
+# a = 520
+# b = 25.5
+# c = 25.0
+
+# name = "Ferid"
+# sentence = "I'm a programmer"
+# sentence = 'This book\'s name is "Under skies"'
+# sentence = 'This books name is This books name is This books name is This books name is \
+# This books name is This books name is This books name is This books name is This books name is \
+# This books name is This books name is This books name is This books name is This books name is This books name is'
+# sentence = 'This is an escape: \\'
+# file_address = 'C:\\Users\\zuley\\Desktop\\Python Lessons\\lesson.py'
+# file_address = R'C:\Users\zuley\Desktop\Python Lessons\lesson.py'
+
+# sentence = "I'm a \nprogrammer"
+# sentence = 'This is an escape: \\n'
+# sentence = "I'm a \\tprogrammer"
+
+# print(sentence)
+
+# a = 52
+# a = 52.5
+# a = 'Name'
+# print(type(a))
+
+# TYPE CONVERSION
+
+# a = '6'
+# b = int(a)
+# print(type(b))
+
+# a = 56
+# b = str(a)
+# print(type(b))
+
+# a = '2.5'
+# b = float(a)
+# print(type(b))
+
+# ''''
+# Escapes:
+
+# \
+# \n
+# \t
+
+# '''
+
+'''
+Operatorlar:
++
+-
+*
+/
+//
+%
+**
+'''
+
+# a = 25
+# b = 2
+# c = a * b
+# print(a ** (1/2))
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+'''
+Assignment operators:
+=
+-=
++=
+*=
+**=
+/=
+//=
+%=
+'''
+# a = 50
+# b = 2
+
+# # a = a / b
+# # a /= b # a = a / b
+# a += 2
+# a **= 2
+# print(a)
+
+'''
+==
+>
+>=
+<=
+<
+!=
+'''
+
+# a = 50
+# b = 51
+
+# print(a == b)
+# print(a > b)
+# print(a != b)
+# print(b >= a)
+
+"""
+Memebership opperators:
+in
+not in
+"""
+
+# letter = 'z'
+# name = 'Ferid'
+
+# print(letter in name)
+# print(letter not in name)
+
+"""
+Logical operators:
+or 
+and 
+not
+"""
+
+# a = 40
+# b = 60
+# print(a > b and a+b==70)
+# print(a < b and a+b==70)
+# print(a < b and a+b==100)
+
+# print(a > b or a+b==70)
+# print(a < b or a+b==70)
+# print(a < b or a+b==100)
+
+# print(not (a < b or a+b==100) )
+
+# name = 'Rufet Telebe'
+# a = 125
+# print(len(name))
+# print(name[-5])
+# a = name[-2]
+# print(a)
+
+#Slice
+
+# name = name[:5]
+# name = name[5:]
+# name = name[::-1]
+# print(name)
+
+# number = 2569
+# number = str(number)
+# reversed_number = number[::-1]
+# reversed_number = int(reversed_number)
+# print(reversed_number)
+
+
+# Boolean
+
+# name = 'Ferid'
+# print(bool(name))
+
+'''
+Boolean deyeri false olan datalar:
+0
+None
+''
+'''
+# a = ' '
+# print(bool(a))
+
+# print(5+6)
+# a = input('Adinizi daxil edin:')
+# print('Istifadecinin adi:', a)
+
+# a = input('Ededi daxil edin:')
+# print(int(a) ** 2)
+
+# surname = 'Hasanova '
+# name = 'Zuleyyxa'
+# full_name = name + " " + surname
+# # a = 3
+# print(full_name)
+
+# Conditions
+
+# a = 50
+# b = 2
+
+# if a > b:
+#     print('a bden boyukdur')
+# if a + b == 100:
+#     print('sert odendi')
+# else:
+#     print('sert odenmedi')
+
+# if a + b == 100:
+#     print('sert odendi')
+# elif a - b == 48:
+#     print('Sert 2 odendi')
+# else:
+#     print('sert odenmedi')
+
+# if a + b == 100:
+#     print('sert odendi')
+# elif a - b == 48:
+#     print('Sert 2 odendi')
+#     if a * 2 == 100:
+#         print('Bu set de odendi')
+# else:
+#     print('sert odenmedi')
+
+
+
+
